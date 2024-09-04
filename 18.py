fdd0426155fd6a396dc979ded6b834e0712a454e
Blevotin <dima.dirys@mail.ru>
2024-09-04 11:13:18 +0000

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,14 +1,16 @@
-class house():
+class House():
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.number = number_of_floors
+        self.number_of_floors = number_of_floors
     def go_to(self, new_floor):
-        if new_floor <= self.number:
+        if new_floor <= 0:
+            return print("Такого этажа нет")
+        if new_floor <= self.number_of_floors:
             for i in range(new_floor):
-                if i < self.number:
+                if i < self.number_of_floors:
                     print(i+1)
         else:
             print("Такого этажа не существует")
 
-a = house("Жк", 23)
+a = House("Жк", 23)
 a.go_to(23)
