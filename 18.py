a7e8bee50385f58acf6cc0b6536e14712539a258
Blevotin <dima.dirys@mail.ru>
2024-09-04 17:50:41 +0300

    Update 18.py

--- 18.py
+++ 18.py
@@ -3,12 +3,11 @@ class House():
         self.name = name
         self.number_of_floors = number_of_floors
     def go_to(self, new_floor):
-        if new_floor <= 0:
+        if new_floor <= 0 or new_floor > self.number_of_floors:
             return print("Такого этажа нет")
         if new_floor <= self.number_of_floors:
-            for i in range(new_floor):
-                if i < self.number_of_floors:
-                    print(i+1)
+            for i in range(1, new_floor+1):
+                    print(i)
         else:
             print("Такого этажа не существует")
 
